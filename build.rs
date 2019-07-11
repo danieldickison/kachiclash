3955228cddd039f7d792acbae83146629c36ae75
Daniel Dickison <git@danieldickison.com>
2019-07-11 09:34:55 -0400

    SCSS compiling

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+use std::io::{self, Write};
+
+fn main() {
+    let output = Command::new("sass")
+        .arg("scss/:public/css/")
+        .output()
+        .expect("failed to execute sass");
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+    if !output.status.success() {
+        println!("warning: sass exit code {}", output.status);
+    }
+}
