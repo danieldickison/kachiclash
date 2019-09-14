14b017986523c29148ceb2ffea5373b3e1029dc5
Daniel Dickison <git@danieldickison.com>
2019-09-14 17:16:18 -0400

    Add basho count down

--- public/js/basho.js
+++ public/js/basho.js
@@ -24,3 +24,37 @@ function savePicks() {
         }
     });
 }
+
+let countDown = document.getElementById('basho-count-down');
+if (countDown) {
+    let startTimestamp = parseInt(countDown.dataset.startDate);
+    let timeSpan = document.getElementById('basho-count-down-time');
+    updateTimeRemaining();
+    setInterval(updateTimeRemaining, 1000);
+
+    function updateTimeRemaining() {
+        let remaining = (startTimestamp - Date.now()) / 1000;
+        let seconds = Math.floor(remaining % 60);
+        let minutes = Math.floor(remaining / 60) % 60;
+        let hours = Math.floor(remaining / 60 / 60) % 24;
+        let days = Math.floor(remaining / 60 / 60 / 24);
+        var str = "";
+
+        if (days > 1) str += days + " days ";
+        else if (days > 0) str += "1 day ";
+
+        if (hours > 1) str += hours + " hours ";
+        else if (hours === 1) str += "1 hour ";
+        else if (days > 0) str += "0 hours ";
+
+        if (minutes > 1) str += minutes + " minutes ";
+        else if (minutes === 1) str += "1 minute ";
+        else if (hours > 0) str += "0 minutes ";
+
+        if (seconds > 1) str += seconds + " seconds ";
+        else if (seconds === 1) str += "1 second ";
+        else if (minutes > 0) str += "0 seconds ";
+
+        timeSpan.innerText = str.trim();
+    }
+}
