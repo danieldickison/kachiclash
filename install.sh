548454fd1d70301d83a6f55dc2bd17af61694f81
Daniel Dickison <git@danieldickison.com>
2019-11-04 08:49:00 -0500

    fix paths

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ cargo build --bin=server --release
 
 sudo rsync -rv public /storage/kachiclash.com/
 sudo chown -R kachiclash:kachiclash /storage/kachiclash.com/public
-sudo chmod 0555 /storage/kachiclash.com/public{,css,img,js}
+sudo chmod 0555 /storage/kachiclash.com/public
+sudo chmod 0555 /storage/kachiclash.com/public/{css,img,js}
 sudo install -v \
     -o kachiclash -g kachiclash -m 0555 \
     target/release/server \
