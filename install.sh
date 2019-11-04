a62e2f93eb536e4e4094c14e756d2eebb472c04e
Daniel Dickison <git@danieldickison.com>
2019-11-04 08:40:56 -0500

    permissions tweak

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cargo build --bin=server --release
 
 sudo rsync -rv public /storage/kachiclash.com/
 sudo chown -R kachiclash:kachiclash /storage/kachiclash.com/public
-sudo chmod -R 0555 /storage/kachiclash.com/public
+sudo chmod 0555 /storage/kachiclash.com/public{,css,img,js}
 sudo install -v \
     -o kachiclash -g kachiclash -m 0555 \
     target/release/server \
