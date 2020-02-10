11f31694e028fb3c142faa219c82ffa9aa8f0782
Daniel Dickison <git@danieldickison.com>
2020-02-10 07:41:29 -0500

    another name mismatch

--- sql/20200210-yurikat.sql
+++ sql/20200210-yurikat.sql
@@ -1,2 +1,3 @@
 UPDATE external_basho_player SET name = 'YurikaT' WHERE name = 'Yurika';
 UPDATE external_basho_player SET name = 'steenbless' WHERE name = 'Ryuden';
+UPDATE external_basho_player SET name = 'steenbless' WHERE name = 'therecanonlybeonedragon';
