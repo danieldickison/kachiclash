4311a6f5cee41ceb5f6d89f47726a6f84d6f360c
Daniel Dickison <git@danieldickison.com>
2020-03-22 12:15:01 -0400

    another player alias

--- sql/20200210-yurikat.sql
+++ sql/20200210-yurikat.sql
@@ -1,3 +1,5 @@
 UPDATE external_basho_player SET name = 'YurikaT' WHERE name = 'Yurika';
 UPDATE external_basho_player SET name = 'steenbless' WHERE name = 'Ryuden';
 UPDATE external_basho_player SET name = 'steenbless' WHERE name = 'therecanonlybeonedragon';
+
+UPDATE external_basho_player SET name = 'Hidoiokami' WHERE name = 'mdshields7';
