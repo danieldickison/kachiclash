2bb84eb02b03f84791a46e404b4fc6df77a31381
Daniel Dickison <git@danieldickison.com>
2021-01-07 21:23:21 -0800

    example updates for setting kyujo

--- sql/20210103-kyujo.sql
+++ sql/20210103-kyujo.sql
@@ -1,1 +1,4 @@
 ALTER TABLE banzuke ADD COLUMN kyujyo INTEGER NOT NULL DEFAULT 0;
+
+# update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Hakuho';
+# update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Wakatakakage';
