3e1ac9a5e06698b690dec67b97158d55a2961f26
Daniel Dickison <git@danieldickison.com>
2021-01-12 21:59:39 -0800

    more kyujyo for 202101

--- sql/20210103-kyujo.sql
+++ sql/20210103-kyujo.sql
@@ -2,3 +2,7 @@ ALTER TABLE banzuke ADD COLUMN kyujyo INTEGER NOT NULL DEFAULT 0;
 
 # update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Hakuho';
 # update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Wakatakakage';
+update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Chiyonokuni';
+update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Chiyotairyu';
+update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Chiyoshoma';
+update banzuke set kyujyo = 1 where basho_id = 202101 and family_name = 'Kaisei';
