be5d6f85265fa0d601e09ec350b035fac1d353c5
Daniel Dickison <git@danieldickison.com>
2022-11-26 02:07:37 +0000

    install script tweaks

--- scripts/install-beta.sh
+++ scripts/install-beta.sh
@@ -5,7 +5,7 @@ cargo build --bin=server --release || exit
 sudo rsync -rv public/ /storage/kachiclash.com/public-beta
 sudo chown -R kachiclash:kachiclash /storage/kachiclash.com/public-beta
 sudo chmod 0555 /storage/kachiclash.com/public-beta
-sudo chmod 0555 /storage/kachiclash.com/public-beta/{css,img,img2,js}
+# sudo chmod 0555 /storage/kachiclash.com/public-beta/{css,img,img2,js}
 sudo install -vb \
     -o kachiclash -g kachiclash -m 0555 \
     target/release/server \
--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,8 @@ cargo build --bin=server --release || exit
 sudo rsync -rv public /storage/kachiclash.com/
 sudo chown -R kachiclash:kachiclash /storage/kachiclash.com/public
 sudo chmod 0555 /storage/kachiclash.com/public
-sudo chmod 0555 /storage/kachiclash.com/public/{css,img,js}
-sudo install -v \
+# sudo chmod 0555 /storage/kachiclash.com/public/{css,img,js}
+sudo install -vb \
     -o kachiclash -g kachiclash -m 0555 \
     target/release/server \
     /storage/kachiclash.com
