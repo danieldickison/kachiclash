a231dbf2d5cfb6bf6accf18e72af06e4d734f1c7
Daniel Dickison <git@danieldickison.com>
2023-07-10 16:54:56 -0700

    merge tyrog accounts

--- sql/merge-players.sql
+++ sql/merge-players.sql
@@ -1,10 +1,10 @@
 BEGIN;
   -- delete old basho data that conflict
-  DELETE FROM basho_result WHERE player_id = 901 AND basho_id = 202109;
-  DELETE FROM pick WHERE player_id = 901 AND basho_id = 202109;
+  DELETE FROM basho_result WHERE player_id = 121 AND basho_id = 202305;
+  DELETE FROM pick WHERE player_id = 121 AND basho_id = 202305;
   
   -- move the "from" player data to the "to" player data.
-  UPDATE OR ROLLBACK pick SET player_id = 901 WHERE player_id = 950;
-  UPDATE OR ROLLBACK award SET player_id = 901 WHERE player_id = 950;
-  UPDATE OR ROLLBACK basho_result SET player_id = 901 WHERE player_id = 950;
+  UPDATE OR ROLLBACK pick SET player_id = 121 WHERE player_id = 2243 AND basho_id = 202305;
+  UPDATE OR ROLLBACK award SET player_id = 121 WHERE player_id = 2243 AND basho_id = 202305;
+  UPDATE OR ROLLBACK basho_result SET player_id = 121 WHERE player_id = 2243 AND basho_id = 202305;
 COMMIT;
