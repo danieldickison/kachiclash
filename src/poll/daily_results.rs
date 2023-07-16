1b4715c78e80fb77fd4a7c0443a31885f911a69c
Daniel Dickison <git@danieldickison.com>
2023-07-16 10:59:30 -0700

    Better polling interval calculation
    Use the current day and the basho.start_date to calculate the polling window so this works regardless of which results have already been loaded.
    
    Also makes error retry use exponential backoff.

--- src/poll/daily_results.rs
+++ src/poll/daily_results.rs
@@ -1,77 +1,44 @@
-use std::time::Duration;
-
 use crate::data::basho::update_torikumi;
 use crate::data::push::mass_notify_day_result;
 use crate::data::BashoId;
 use crate::data::RankDivision;
 use crate::external::sumo_api;
 use crate::AppState;
-use chrono::{DurationRound, Utc};
-use chrono::{FixedOffset, NaiveTime};
-use tokio::time::{interval, sleep};
+use chrono::Utc;
+use chrono::{DateTime, FixedOffset, NaiveTime};
+use tokio::time::sleep;
 
-const POLL_INTERVAL: u64 = 300; // 5 min
-const POLL_DURATION: i64 = 3600; // 1 hour
+const POLL_INTERVAL_FAST: i64 = 300; // 5 min
+const POLL_INTERVAL_SLOW: i64 = 3600; // 1 hour
+const POLL_FAST_DURATION: i64 = 3600; // 1 hour
+const ERROR_RETRY_WAIT: i64 = 15;
+const ERROR_RETRY_MULTIPLIER: i32 = 2;
+
+lazy_static! {
+    static ref JST: FixedOffset = FixedOffset::east_opt(9 * 60 * 60).unwrap();
+    static ref POLL_START_TIME_JST: NaiveTime = NaiveTime::from_hms_opt(18, 0, 0).unwrap();
+}
 
 pub async fn daily_results(app_state: AppState) -> ! {
-    lazy_static! {
-        static ref JST: FixedOffset = FixedOffset::east_opt(9 * 60 * 60).unwrap();
-        static ref POLL_START_TIME: NaiveTime = NaiveTime::from_hms_opt(18, 0, 0).unwrap();
-        static ref POLL_END_TIME: NaiveTime = NaiveTime::from_hms_opt(20, 0, 0).unwrap();
-    }
-    let mut poll = interval(Duration::from_secs(POLL_INTERVAL));
-    poll.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
-
+    let mut retry_wait = chrono::Duration::seconds(ERROR_RETRY_WAIT);
     loop {
-        let now_jst = Utc::now().with_timezone(&*JST);
-        let today_poll_start = now_jst
-            .date_naive()
-            .and_time(*POLL_START_TIME)
-            .and_local_timezone(*JST)
-            .unwrap();
-        let today_poll_end = today_poll_start + chrono::Duration::seconds(POLL_DURATION);
-
-        // sleep until 6pm JST when final results might be ready
-        if now_jst < today_poll_start {
-            let sleep_dur = today_poll_start.signed_duration_since(now_jst);
-            debug!(
-                "sleeping for {} until {}",
-                PrettyDuration(sleep_dur),
-                today_poll_start
-            );
-            sleep(sleep_dur.to_std().unwrap()).await;
-        }
-
-        // poll until 7pm JST by which time final results should've arrived
-        let mut done = false;
-        while !done && Utc::now() < today_poll_end {
-            poll.tick().await;
-            match do_tick(&app_state).await {
-                Ok(true) => done = true,
-                Ok(false) => debug!(
-                    "final results have not arrived yet; sleeping for {:?}",
-                    poll.period()
-                ),
-                Err(e) => error!("do_tick failed: {}", e),
+        let next_poll_datetime = match do_tick(&app_state).await {
+            Ok(date) => {
+                retry_wait = chrono::Duration::seconds(ERROR_RETRY_WAIT);
+                date
             }
-        }
+            Err(e) => {
+                error!("do_tick error: {}", e);
+                retry_wait = retry_wait * ERROR_RETRY_MULTIPLIER; // exponential backoff
+                Utc::now() + retry_wait
+            }
+        };
 
-        if !done {
-            error!(
-                "Failed to get final results from sumo-api before timeout {}",
-                today_poll_end
-            );
-        }
-
-        // sleep until midnight JST
-        let midnight = (today_poll_start + chrono::Days::new(1))
-            .duration_trunc(chrono::Duration::days(1))
-            .unwrap();
-        let sleep_dur = midnight.signed_duration_since(Utc::now());
+        let sleep_dur = next_poll_datetime.signed_duration_since(Utc::now());
         debug!(
-            "sleeping for {:.3} until midnight {}",
-            PrettyDuration(sleep_dur),
-            midnight
+            "sleeping for {} until next poll at {}",
+            PrettyDuration::from(sleep_dur),
+            next_poll_datetime
         );
         sleep(sleep_dur.to_std().unwrap()).await;
     }
@@ -96,18 +63,26 @@ impl std::fmt::Display for PrettyDuration {
         f.write_fmt(format_args!("{:.3}s", secs as f32 / 1000.0))
     }
 }
+impl From<chrono::Duration> for PrettyDuration {
+    fn from(value: chrono::Duration) -> Self {
+        Self(value)
+    }
+}
 
-async fn do_tick(app_state: &AppState) -> anyhow::Result<bool> {
+/// Checks sumo-api for next day's results. Returns the time at which we should poll.
+async fn do_tick(app_state: &AppState) -> anyhow::Result<DateTime<Utc>> {
     trace!("daily_results tick starting");
     let basho_id: BashoId;
+    let basho_start_date: DateTime<Utc>;
     let last_day: u8;
     {
         // Find the last completed day of the currently active basho. The current basho should be the only one in the db with picks but without any associated awards.
         let db = app_state.db.lock().unwrap();
-        (basho_id, last_day) = db.query_row(
+        (basho_id, basho_start_date, last_day) = db.query_row(
             "
             SELECT
                 basho.id,
+                basho.start_date,
                 COALESCE((SELECT MAX(day) FROM torikumi WHERE torikumi.basho_id = basho.id), 0)
             FROM basho
             WHERE
@@ -115,17 +90,17 @@ async fn do_tick(app_state: &AppState) -> anyhow::Result<bool> {
                 AND EXISTS (SELECT 1 FROM pick WHERE pick.basho_id = basho.id)
             ", // This will error if multiple rows are returned
             (),
-            |row| Ok((row.get(0)?, row.get(1)?)),
+            |row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
         )?;
     }
 
     let day = last_day + 1;
-    debug!("querying sumo-api for basho {} day {}", basho_id.id(), day);
+    debug!("Querying sumo-api for basho {} day {}", basho_id.id(), day);
     let resp = sumo_api::BanzukeResponse::fetch(basho_id, RankDivision::Makuuchi).await?;
     if resp.day_complete(day) {
         let update_data = resp.torikumi_update_data(day);
         info!(
-            "got complete day {} results; updating db with {} bouts",
+            "Got complete day {} results; updating db with {} bouts",
             day,
             update_data.len()
         );
@@ -142,8 +117,30 @@ async fn do_tick(app_state: &AppState) -> anyhow::Result<bool> {
             day,
         )
         .await?;
-        Ok(true)
+
+        Ok(next_poll_date(basho_start_date, day + 1))
     } else {
-        Ok(false)
+        debug!("Day {} results incomplete; going to sleep", day);
+        Ok(next_poll_date(basho_start_date, day))
+    }
+}
+
+fn next_poll_date(basho_start_date: DateTime<Utc>, day: u8) -> DateTime<Utc> {
+    let poll_start = basho_start_date
+        .with_timezone(&*JST)
+        .date_naive()
+        .checked_add_days(chrono::Days::new(day as u64 - 1))
+        .unwrap()
+        .and_time(*POLL_START_TIME_JST)
+        .and_local_timezone(*JST)
+        .unwrap();
+
+    let now = Utc::now().with_timezone(&*JST);
+    if now < poll_start {
+        poll_start.with_timezone(&Utc)
+    } else if now < poll_start + chrono::Duration::seconds(POLL_FAST_DURATION) {
+        now.with_timezone(&Utc) + chrono::Duration::seconds(POLL_INTERVAL_FAST)
+    } else {
+        now.with_timezone(&Utc) + chrono::Duration::seconds(POLL_INTERVAL_SLOW)
     }
 }
