25b70cf3ce0ad9f0fe83be64d38c383db6721f6f
Daniel Dickison <git@danieldickison.com>
2023-08-20 18:29:53 -0700

    rename basho_alert to basho_prelude

--- src/poll/mod.rs
+++ src/poll/mod.rs
@@ -1,14 +1,14 @@
 use crate::AppState;
 use tokio::task::spawn;
 
-pub mod basho_alert;
-use basho_alert::basho_alert;
+pub mod basho_prelude;
+use basho_prelude::basho_prelude;
 
 pub mod daily_results;
 use daily_results::daily_results;
 
 pub fn start(app_state: &AppState) -> anyhow::Result<()> {
-    spawn(basho_alert(app_state.clone()));
+    spawn(basho_prelude(app_state.clone()));
     spawn(daily_results(app_state.clone()));
     Ok(())
 }
