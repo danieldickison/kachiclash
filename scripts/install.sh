8dafedd87baabf37bc2d41543efb59d33dca9959
Daniel Dickison <git@danieldickison.com>
2024-08-03 14:21:21 -0700

    Use install.sh --beta to install beta build

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
+KC_HOME=${KC_HOME:-/home/kachiclash}
+PUBLIC=$KC_HOME/public
+SERVER=$KC_HOME/server
+SERVICE=kachiclash
+
+case $1 in
+--beta)
+    PUBLIC=$KC_HOME/public-beta
+    SERVER=$KC_HOME/server-beta
+    SERVICE=kachiclash-beta
+    ;;
+'') ;;
+*)
+    echo "Unknown option: $1"
+    exit 3
+    ;;
+esac
 
 cargo build --bin=server --release --locked || exit
 
-sudo rsync -rv public /home/kachiclash/
-sudo chown -R kachiclash:nogroup /home/kachiclash/public
-sudo chmod 0555 /home/kachiclash/public
+sudo rsync -rv public/ $PUBLIC
+sudo chown -R kachiclash:nogroup $PUBLIC
+sudo chmod 0555 $PUBLIC
 # sudo chmod 0555 /storage/kachiclash.com/public/{css,img,js}
 sudo install -vb \
     -o kachiclash -g nogroup -m 0555 \
     target/release/server \
-    /home/kachiclash
+    $SERVER
 
-sudo systemctl restart kachiclash
-sudo systemctl restart kachiclash-levelone
+sudo systemctl restart $SERVICE
