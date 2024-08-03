1a62dadf626cfd0c2b1984e77ce8e30917f12319
Daniel Dickison <git@danieldickison.com>
2024-08-03 14:48:34 -0700

    Restore sudo for backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,4 @@ set -e
 
 VAR=${KC_VAR:-/home/kachiclash/public/var}
 DATE=`date +%Y%m%d`
-mkdir -p $VAR/backup
-#sudo -u kachiclash
-tar --zstd -cf $VAR/backup/dbs-$DATE.tar.zstd $VAR/*.sqlite
+sudo -u kachiclash tar --zstd -cf $VAR/backup/dbs-$DATE.tar.zstd $VAR/*.sqlite
