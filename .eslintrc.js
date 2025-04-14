13f2fe72dcdd9ee9fed94ed38b20290f5c17a04a
Daniel Dickison <git@danieldickison.com>
2025-04-14 00:10:39 -0700

    Add prettierignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: 'tsconfig.json',
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
   },
   rules: {},
 };
